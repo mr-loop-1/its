77e201581beb3bbfd525a9a4d2506ee45b7e1563
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-20 17:13:58 +0530

    add: config adds

--- server/config/settingStore.js
+++ server/config/settingStore.js
@@ -1,4 +1,24 @@
 const config = {
-    accessLevel: {},
-    bugs: {},
+    accessLevel: {
+        accessCode: {
+            ADMIN: 1,
+            MANAGER: 2,
+            MEMBER: 3,
+        },
+        accessEnum: [ADMIN, MANAGER, MEMBER],
+        accessCodeEnum: [1, 2, 3],
+    },
+    bugs: {
+        statusCode: {
+            OPEN: 1,
+            TRIAGE: 2,
+            IN_PROGRESS: 3,
+            REVIEW_REQUIRED: 4,
+            CLOSED: 5,
+        },
+        statusEnum: [OPEN, TRIAGE, IN_PROGRESS, REVIEW_REQUESTED, CLOSED],
+        statusCodeEnum: [1, 2, 3, 4, 5],
+    },
 };
+
+module.exports = config;
