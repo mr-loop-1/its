3099627d92fd2ae49e6e4a01f76a6ca698ed83bd
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-23 02:32:48 +0530

    add: invite routes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -18,3 +18,36 @@ exports.getInvites = async (req, res) => {
 
     return res.json(data);
 };
+
+exports.sendInvite = async (req, res) => {
+    const user = req.user;
+    const body = req.body;
+    const createInvite = new invitesModel({
+        invited: body.invited,
+        invitedBy: user.id,
+        projectId: body.projectId,
+        status: false,
+    });
+    const newInvite = await createInvite.save();
+    return res.json("CREATED INV");
+};
+
+exports.acceptInvite = async (req, res) => {
+    const user = req.user;
+    const params = req.params;
+    const documents = await invitesModel.findByIdAndUpdate(params.inviteId, {
+        status: true,
+    });
+
+    return res.json("DONEINVITE");
+};
+
+exports.rejectInvite = async (req, res) => {
+    const user = req.user;
+
+    const documents = await invitesModel.findByIdAndUpdate(params.inviteId, {
+        status: false,
+    });
+
+    return res.json("REMOVED INV");
+};
