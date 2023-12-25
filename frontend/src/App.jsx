919fbc5994f73fe570fe13c884ce6b536b8747dd
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-25 13:32:06 +0530

    add: added project sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Sidebar from './components/sidebar';
+import ListBar from './components/listBar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
     <>
       <h1>asd</h1>
       <Sidebar />
+      <ListBar />
     </>
   );
 }
