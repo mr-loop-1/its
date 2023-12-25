185965044999be35a1c499dd6256cc17d874bc08
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-26 02:13:15 +0530

    add: guard added

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Project from './pages/projects';
 import Register from './pages/auth/register';
 import Login from './pages/auth/login';
+import RouteGuard from './guard/route';
 
 export default function Routing() {
   return (
@@ -10,10 +11,10 @@ export default function Routing() {
       <Routes>
         <Route path="/register" Component={Register} />
         <Route path="/login" Component={Login} />
-        <Route path="/" Component={Project} />
-
-        <Route path="projects/*" Component={Project} />
-        <Route path="invites/*" Component={Project} />
+        <Route element={<RouteGuard />}>
+          <Route path="projects/*" Component={Project} />
+          <Route path="invites/*" Component={Project} />
+        </Route>
       </Routes>
     </Router>
   );
