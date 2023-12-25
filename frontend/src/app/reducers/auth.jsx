4a914f32f53239388df1c8aa54438bfff6d300df
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-26 00:50:11 +0530

    add: route redux

---
+++ frontend/src/app/reducers/auth.jsx
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { registerUser } from '../actions/auth';
+
+const initialState = {
+  loading: false,
+  userInfo: {}, // for user object
+  userToken: null, // for storing the JWT
+  error: null,
+  success: false, // for monitoring the registration process.
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    // register user
+    [registerUserUser.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.success = true; // registration successful
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
+});
+
+export default authSlice.reducer;
