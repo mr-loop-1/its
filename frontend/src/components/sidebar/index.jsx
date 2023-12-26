4319e6812987ea14c1dc5e11f2c0ba56e9c899a7
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-26 23:52:00 +0530

    testing done

--- frontend/src/components/sidebar/index.jsx
+++ frontend/src/components/sidebar/index.jsx
@@ -4,6 +4,16 @@ import { NavLink } from 'react-router-dom';
 export default function Sidebar() {
   return (
     <div className="z-50 w-[64px] h-full bg-[#0747A6] fixed overflow-x-hidden transition-all duration-[0.1s] left-0 top-0 hover:w-[200px] hover:shadow-[0_0_50px_0_rgba(0,0,0,0.6)] transform translate-z-0">
+      <div
+        className="relative w-full h-[42px] pl-[64px] text-[#deebff] text-lg"
+        // onClick={createProjectOpen}
+      >
+        <img
+          src="/plus.svg"
+          className="absolute left-[18px] w-[27px] h-[27px]"
+        />
+        Create Issue
+      </div>
       <a
         href="/"
         className="block relative transition-[left] duration-[0.1s] mt-5 mb-2.5 mx-0 left-0"
