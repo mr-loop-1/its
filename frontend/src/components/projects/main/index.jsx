f364a1ff61dd0b9ace77aa22f532f2aacdcf5b2d
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-28 02:54:31 +0530

    add: added invite button

--- frontend/src/components/projects/main/index.jsx
+++ frontend/src/components/projects/main/index.jsx
@@ -33,14 +33,13 @@ export default function ProjectMain() {
 
   return (
     //! very imp right-0
-    <div className="fixed right-0 left-72 lg:left-[448px] mt-8 mx-8 lg:mx-16 overflow-hidden ">
+    <div className="fixed right-0 left-72 lg:left-[448px] mt-8 mx-8 pr-10 lg:mx-16 overflow-hidden ">
       {isLoading ? (
         <ReloadIcon className=" h-40 w-40 animate-spin" />
       ) : (
         <div className="w-full flex flex-col">
           <ProjectTopNav title={project.title} id={project.id} />
           <ProjectHeader project={project} />
-          <div className="self-end">asdhkjn</div>
         </div>
       )}
     </div>
--- frontend/src/components/projects/main/topNav.jsx
+++ frontend/src/components/projects/main/topNav.jsx
@@ -1,12 +1,18 @@
+import { Pencil2Icon, TrashIcon } from '@radix-ui/react-icons';
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export default function ProjectTopNav({ title, id }) {
   return (
-    <div className="">
-      <span className="text-gray-500 text-sm">
+    <div className="w-full">
+      <div className="text-gray-500 text-sm">
         Projects /<Link to={`/projects/${id}`}>{title}</Link>
-      </span>
+      </div>
+      <div className="w-full text-4xl font-bold leading-tight flex">
+        {title}
+        <Pencil2Icon className="ml-auto w-10 h-10 cursor-pointer" />
+        <TrashIcon className="w-10 h-10 ml-5 text-red-700 cursor-pointer" />
+      </div>
     </div>
   );
 }
