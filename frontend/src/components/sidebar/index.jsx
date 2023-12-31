023fe8c7acf60ee93691e5b8109f8f4567d1149c
mr-loop-1 <loopolecreator1@gmail.com>
2023-12-31 18:45:54 +0530

    add: sidebar done

--- frontend/src/components/sidebar/index.jsx
+++ frontend/src/components/sidebar/index.jsx
@@ -1,64 +1,86 @@
 import React from 'react';
 import clsx from 'clsx';
-import { Link, NavLink, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { HomeIcon } from '@radix-ui/react-icons';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  BackpackIcon,
+  Crosshair2Icon,
+  ExitIcon,
+  HomeIcon,
+} from '@radix-ui/react-icons';
 import { Avatar, AvatarFallback, AvatarImage } from './avatar';
+import { setUser } from '@/app/reducers/auth';
 
 export default function Sidebar() {
   const location = useLocation();
   const user = useSelector((state) => state.auth.userInfo);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  /**
-   * w-16 initial
-   * 80% hover on small screen
-   * w-48 hover on large screen
-   */
+  const handleLogout = async () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    dispatch(setUser({ user: null, token: null }));
+    // setTimeout(() => {
+    //   dispatch(setUser({ user: null, token: null }));
+    // }, 1000);
+    navigate('/login');
+  };
 
   return (
     <div
       className={clsx(
         ['/register', '/login'].includes(location.pathname) && 'hidden',
-        'fixed z-50 left-0 top-0 w-16 hover:w-[80%] md:hover:w-48 h-screen bg-[#0747A6] overflow-x-hidden transition-all duration-[0.1s] hover:shadow-[0_0_50px_0_rgba(0,0,0,0.6)] transform translate-z-0',
+        'fixed flex flex-col justify-between z-50 left-0 top-0 w-16 hover:w-[80%] md:hover:w-48 h-screen bg-[#0747A6] overflow-x-hidden transition-all duration-[0.1s] hover:shadow-[0_0_50px_0_rgba(0,0,0,0.6)] transform translate-z-0',
       )}
     >
-      <img
-        src={`/profile/${user.slug}.svg`}
-        alt="profile image"
-        className="w-12 mt-5 ml-2"
-      />
-      {/* <img
-        src={`/profile/${user.slug}.svg`}
-        alt="profile image"
-        className="w-[80%] mt-5 mx-auto"
-      /> */}
-
-      {/* <div className='flex flex-col items-center'>  */}
-
-      <div className="mt-20 md:mt-60 py-3 rounded flex items-center w-full hover:brightness-125 cursor-pointer">
-        <span className="w-16">
-          <HomeIcon className="w-8 h-8 ml-4 text-white" />
-        </span>
-        <span className="text-white font-semibold ml-4">
-          <Link to="/">Home</Link>
-        </span>
+      <div>
+        <img
+          src={`/profile/${user.slug}.svg`}
+          alt="profile image"
+          className="w-12 mt-5 ml-2"
+        />
       </div>
-      {/* </div> */}
-      {/* <Avatar className="">
-        <AvatarImage src={`/profile/${user.slug}.svg`} alt="profile image" />
-        <AvatarFallback>{user.name.split(' ')[0]}</AvatarFallback>
-      </Avatar> */}
-      {/* <img
-        className="w-10  mt-8 group-hover:w-28 "
-        src={`/profile/${user.slug}.svg`}
-      />
-      <div className="hidden p-3 group-hover:block text-white font-semibold text-center">
-        {user.name}
+      <div>
+        <Link to="/">
+          <div className="py-3 rounded flex items-center w-full hover:bg-[#1a52a7] active:hover:bg-[#1a52a7] cursor-pointer">
+            <span className="w-16">
+              <HomeIcon className="w-8 h-8 ml-4 text-white" />
+            </span>
+            <span className="text-white font-semibold ml-4">Home</span>
+          </div>
+        </Link>
+        <Link to="/projects">
+          <div className="py-3 rounded flex items-center w-full hover:bg-[#1a52a7] active:bg-[#1a52a7] cursor-pointer">
+            <span className="w-16">
+              <BackpackIcon className="w-8 h-8 ml-4 text-white" />
+            </span>
+            <span className="text-white font-semibold ml-4">Projects</span>
+          </div>
+        </Link>
+        <Link to="/bugs">
+          <div className="py-3 rounded flex items-center w-full hover:bg-[#1a52a7] active:bg-[#1a52a7] cursor-pointer">
+            <span className="w-16">
+              <Crosshair2Icon className="w-8 h-8 ml-4 text-white" />
+            </span>
+            <span className="text-white font-semibold ml-4">Bugs</span>
+          </div>
+        </Link>
       </div>
 
-      <div className="group overflow-hidden text-white font-semibold">
-        <HomeIcon className="w-3 group-hover:w-10 text-white" />
-      </div> */}
+      <div>
+        {/* <Link to="/"> */}
+        <div
+          className="mb-10 rounded flex items-center w-full hover:bg-[#1a52a7] active:bg-[#1a52a7] cursor-pointer"
+          onClick={handleLogout}
+        >
+          <span className="w-16">
+            <ExitIcon className="w-8 h-8 ml-4 text-white" />
+          </span>
+          <span className="text-white font-semibold ml-4">Logout</span>
+        </div>
+        {/* </Link> */}
+      </div>
     </div>
   );
 }
